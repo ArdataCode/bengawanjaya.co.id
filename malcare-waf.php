58b0f4e1002e2a4aa32ecb6ccca0852422bde995
ArdataCode <ardata.indonesia@gmail.com>
2024-05-15 15:19:25 +0700

    pertama

---
+++ malcare-waf.php
@@ -0,0 +1,8 @@
+<?php
+// Please validate auto_prepend_file setting before removing this file
+
+if (file_exists('/home/u1567848/public_html/bengawanjaya.co.id/wp-content/plugins/malcare-security/protect/prepend/ignitor.php')) {
+	define("MCDATAPATH", '/home/u1567848/public_html/bengawanjaya.co.id/wp-content/mc_data/');
+	define("MCCONFKEY", 'f39abe1c6e4cd6f06f98f64ef44f7255');
+	include_once('/home/u1567848/public_html/bengawanjaya.co.id/wp-content/plugins/malcare-security/protect/prepend/ignitor.php');
+}
